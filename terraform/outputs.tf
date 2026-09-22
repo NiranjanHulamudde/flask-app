1d47e4ed48882126b9453fb0b24d93e6e62360cb
Niranjan Hulamudde <niranjan.hulamudde123@gmail.com>
2026-09-22 12:52:57 +0530

    Change output value from public_ip to arn

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,6 @@ output "instance_public_ip" {
 	description = "instance public ip"
 }
 output "instance_arn" {
-	value = aws_instance.flask-service.public_ip
+	value = aws_instance.flask-service.arn
 	description = "instance arn"
 }
